fd90031824ef77dc2276b2eeed38db5b5f739bca
MSpilari <mb.spilari@hotmail.com>
2020-05-15 18:05:59 -0300

    formatando o script

--- Beginner/Calculator/scripts.js
+++ Beginner/Calculator/scripts.js
@@ -1,50 +1,71 @@
 const display = document.querySelector('span.display')
 const keypad = document.querySelector('.buttons')
 
-let firstNumber = null
-let operator = null
-let secondNumber = null
-let result = null
-
+let counter = 0
+const pResults = ['0']
 
 function chooseOperator (symbol) {
-    if(symbol == '+') return result = firstNumber + secondNumber
-    if(symbol == '-') return result = firstNumber - secondNumber
-    if(symbol == '*') return result = firstNumber * secondNumber
-    if(symbol == '/') return result = firstNumber / secondNumber
+    if(symbol == '+') return result = Number(pResults[1]) + Number(pResults[2])
+    
+    if(symbol == '-') return result = Number(pResults[1]) - Number(pResults[2])
+    
+    if(symbol == '*') { return result = (Number(pResults[1]) * Number(pResults[2])) % 1 == 0 
+                                            ? Number(pResults[1]) * Number(pResults[2]) 
+                                            : (Number(pResults[1]) * Number(pResults[2])).toFixed(2)
+    }
+    if(symbol == '/') { return result = (Number(pResults[1]) / Number(pResults[2])) % 1 == 0 
+                                            ? Number(pResults[1]) / Number(pResults[2]) 
+                                            : (Number(pResults[1]) / Number(pResults[2])).toFixed(2)
+
+    }
+}
+
+function possibleResult(displayShow){    
+    return pResults.includes(displayShow)
+}
+
+function isValid(button) {
+    if(button.classList.contains('number') || button.classList.contains('dot')) {
+        if(counter < 9)    
+            return true
+    }
+}
+
+function countingNumbers() {
+    if(display.innerHTML != 0 && counter < 9) {
+        counter += 1
+    }
 }
 
 
 keypad.addEventListener('click', (event) => {
     
     let clicked = event.target
-
     
-    if( clicked.classList.contains('number') || 
-        clicked.classList.contains('dot') ) {
-        
-        if( display.innerHTML == 0 || 
-            display.innerHTML == result ||
-            display.innerHTML == firstNumber)
-            {
-            
-            display.innerHTML = clicked.value
-
+    if(isValid(clicked)) {
+    
+        if(possibleResult(display.innerHTML)){ 
+             display.innerHTML = clicked.value
+             countingNumbers()
         }
         else{
             display.innerHTML += clicked.value
+            countingNumbers()
         }
     }
-
+    console.log(counter)
     if(clicked.classList.contains('operator')){
-        firstNumber = Number(display.innerHTML)
+        counter = 0
+        pResults[1] = display.innerHTML
         operator = clicked.value
+        console.log(pResults)
     }
 
     if(clicked.value == '='){
-        secondNumber = Number(display.innerHTML)
-        chooseOperator(operator)
-        display.innerHTML = result
+        counter = 0
+        pResults[2] = display.innerHTML
+        display.innerHTML = chooseOperator(operator)
+        pResults[3] = String(chooseOperator(operator))
     }
 })
 
