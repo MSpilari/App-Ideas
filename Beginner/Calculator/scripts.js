c628abf4f45f2092b4c3a1efab3be15d15e1f02a
MSpilari <mb.spilari@hotmail.com>
2020-05-19 12:09:55 -0300

    Finalizando a refatoração e o projeto

--- Beginner/Calculator/scripts.js
+++ Beginner/Calculator/scripts.js
@@ -1,103 +1,96 @@
-const display = document.querySelector('span.display')
-const keypad = document.querySelector('.buttons')
+const displayCalc = document.querySelector('span.display')
+const buttonsCalc = document.querySelector('.buttons')
 
-let counter = 0
-const pResults = ['0', 'ERR']
+let firstValue = null
+let operator = null
+let secondValue = null
+let result = null
 
-function setResult (symbol) {
-    if(symbol == '+') return result = Number(pResults[2]) + Number(pResults[3])
-    
-    if(symbol == '-') return result = Number(pResults[2]) - Number(pResults[3])
-    
-    if(symbol == '*') { return result = (Number(pResults[2]) * Number(pResults[3])) % 1 == 0 
-                                            ? Number(pResults[2]) * Number(pResults[3]) 
-                                            : (Number(pResults[2]) * Number(pResults[3])).toFixed(2)
-    }
-    if(symbol == '/') { return result = (Number(pResults[2]) / Number(pResults[3])) % 1 == 0 
-                                            ? Number(pResults[2]) / Number(pResults[3]) 
-                                            : (Number(pResults[2]) / Number(pResults[3])).toFixed(2)
+function btnType(value) {
+    const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    const operators = ['+', '-', '*', '/', '=']
+    const dot = ['.']
+    const specialValues = ['AC', 'C']
 
-    }
+    if (digits.includes(value)) return 'digit'
+    if (operators.includes(value)) return 'operator'
+    if (dot.includes(value)) return 'dot'
+    if (specialValues.includes(value)) return 'special'
 }
 
-function possibleResult(displayShow){    
-    return pResults.includes(displayShow)
-}
-
-function isValid(button) {
-    if(button.classList.contains('number') || button.classList.contains('dot')) {
-        if(counter < 8)    
-            return true
-    }
-}
-
-function countingNumbers() {
-    if(display.innerHTML != '0' || display.innerHTML == '0.' && counter <= 8) {
-        counter += 1
-    }
-}
-
-function backspace (){
-    counter -= 1
-    let arr = display.innerHTML
-    let narr = arr.slice(0, arr.length -1)
-    if(narr == []) {
-        display.innerHTML = 0
-    }
-    else {
-        display.innerHTML = narr
-    }
-}
-
-function allClear() {
-    counter = 0
-    pResults[2] = ''
-    pResults[3] = ''
-    pResults[4] = ''
-    display.innerHTML = 0
+function showResult(fValue, Op, sValue) {
+    if (Op == "+") return result = Number(fValue) + Number(sValue)
+    if (Op == "-") return result = Number(fValue) - Number(sValue)
+    if (Op == "*") return result = Number(fValue) * Number(sValue)
+    if (Op == "/") return result = Number(fValue) / Number(sValue)
+    if (Op == null) return result = '0'
 }
 
 
-keypad.addEventListener('click', (event) => {
-    
-    let clicked = event.target
+buttonsCalc.addEventListener('click', event => {
+    let btnClicked = event.target.innerHTML
 
-    if(isValid(clicked)) {
-    
-        if (display.innerHTML == '0' && clicked.value == '.') {
-            display.innerHTML += clicked.value
-            countingNumbers()
+    if ( btnType(btnClicked) == 'dot' && !displayCalc.innerHTML.includes('.')) {
+        
+            displayCalc.innerHTML += btnClicked
+    }
+
+    if (btnType(btnClicked) == 'digit') {
+        if (displayCalc.innerHTML == firstValue || displayCalc.innerHTML == result) {
+            displayCalc.innerHTML = '0'
         }
-        else if(possibleResult(display.innerHTML)){ 
-             display.innerHTML = clicked.value
-             countingNumbers()
-        }
-        else{
-            display.innerHTML += clicked.value
-            countingNumbers()
+        displayCalc.innerHTML == '0' ?
+            displayCalc.innerHTML = btnClicked :
+            displayCalc.innerHTML += btnClicked
+    }
+
+    if (btnType(btnClicked) == 'operator') {
+        if (firstValue == null && operator == null && btnClicked != '=') {
+            firstValue = displayCalc.innerHTML
+            operator = btnClicked
+        } 
+        else if (firstValue != null && operator != null && btnClicked == '=') {
+            secondValue = displayCalc.innerHTML
+            displayCalc.innerHTML = showResult(firstValue, operator, secondValue) != undefined ?
+                showResult(firstValue, operator, secondValue) :
+                '0'
+            firstValue = showResult(firstValue, operator, secondValue)
+            operator = null
+        } 
+        else if (firstValue != null && operator == null && btnClicked != '=') {
+            operator = btnClicked
+        } 
+        else if (firstValue == null && operator == null && btnClicked == '=') {
+            return displayCalc.innerHTML = 0
+        } 
+        else {
+            secondValue = displayCalc.innerHTML
+            newFirstValue = showResult(firstValue, operator, secondValue)
+            displayCalc.innerHTML = showResult(firstValue, operator, secondValue)
+            firstValue = newFirstValue
+            operator = btnClicked
         }
     }
 
-    if(clicked.classList.contains('operator')){
-        counter = 0
-        pResults[2] = display.innerHTML
-        operator = clicked.value
-    }
-
-    if(clicked.value == '='){
-        counter = 0
-        pResults[3] = display.innerHTML
-        display.innerHTML = String(setResult(operator)).length <= 8 ? setResult(operator) : 'ERR'
-        pResults[4] = String(setResult(operator))
-    }
-
-    if(clicked.value == 'C') {
-        backspace()
-    }
-
-    if (clicked.value == 'AC'){
-        allClear()        
+    if (btnType(btnClicked) == 'special') {
+        if (btnClicked == 'C') {
+            let newDisplay = displayCalc.innerHTML.slice(0, displayCalc.innerHTML.length - 1)
+            newDisplay.length == 0 ?
+                displayCalc.innerHTML = '0' :
+                displayCalc.innerHTML = newDisplay
+            if(displayCalc.innerHTML == '0' && operator != null){
+                operator = null
+                firstValue != null ? 
+                displayCalc.innerHTML = firstValue : 
+                displayCalc.innerHTML = '0'
+            }    
+        }
+        else {
+            firstValue = null
+            operator = null
+            secondValue = null
+            result = null
+            displayCalc.innerHTML = '0'
+        }
     }
 })
-
-
